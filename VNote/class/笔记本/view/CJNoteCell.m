6f18ce4e707799af6f6502d9e70af0180459167c
ccj <2810418812@qq.com>
2019-03-20 11:57:32 +0800

    update

---
+++ VNote/class/笔记本/view/CJNoteCell.m
@@ -0,0 +1,24 @@
+//
+//  CJNoteCell.m
+//  VNote
+//
+//  Created by ccj on 2019/3/19.
+//  Copyright © 2019 ccj. All rights reserved.
+//
+
+#import "CJNoteCell.h"
+
+@implementation CJNoteCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
