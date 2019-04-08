863109346fad80b17a1330cd4f6b0a8a8abd10f7
ccj <2810418812@qq.com>
2019-04-09 00:14:09 +0800

    update

--- VNote/class/公共/CJConfig.h
+++ VNote/class/公共/CJConfig.h
@@ -135,7 +135,7 @@ form.submit();\
 }"
 
 
-#define APP_ID @""
+#define APP_ID @"1459104946"
 // iOS 11 以下的评价跳转
 #define APP_OPEN_EVALUATE [NSString stringWithFormat:@"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@", APP_ID]
 // iOS 11 的评价跳转
